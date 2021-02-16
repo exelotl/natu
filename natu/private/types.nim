236b3aa21a059b693edbc7215304e0c9ae6a2907
Jeremy Clarke <geckojsc@gmail.com>
2021-02-16 01:33:54 +0000

    Use completeStruct pragma, remove _size constants

--- natu/private/types.nim
+++ natu/private/types.nim
@@ -2,7 +2,7 @@
 # ==========================
 
 type
-  FnPtr* = proc () {.nimcall.}    ## void foo() function pointer, used for interrupt handlers etc.
+  FnPtr* = proc () {.nimcall.}    ## Function pointer, used for interrupt handlers etc.
 
 # To be used with codegenDecl pragma:
 const
@@ -14,7 +14,7 @@ const
 
 
 type
-  Block* {.importc: "BLOCK", header: "tonc.h", bycopy.} = object
+  Block* {.importc: "BLOCK", header: "tonc.h", bycopy, completeStruct.} = object
     ## 8-word type for fast struct-copies
     data* {.importc: "data".}: array[8, uint32]
 
@@ -31,11 +31,11 @@ type
 type
   Tile* = Tile4
   
-  Tile4* {.importc: "TILE4", header: "tonc.h", bycopy.} = object
+  Tile4* {.importc: "TILE4", header: "tonc.h", bycopy, completeStruct.} = object
     ## 4bpp tile type, for easy indexing and copying of 4-bit tiles
     data* {.importc: "data".}: array[8, uint32]
     
-  Tile8* {.importc: "TILE8", header: "tonc.h", bycopy.} = object
+  Tile8* {.importc: "TILE8", header: "tonc.h", bycopy, completeStruct.} = object
     ## 8bpp tile type, for easy indexing and copying of 8-bit tiles
     data* {.importc: "data".}: array[16, uint32]
 
@@ -45,14 +45,14 @@ type
   ObjAffineDest* = AffDst
   BgAffineDest* = AffDstEx
   
-  AffSrc* {.importc: "AFF_SRC", header: "tonc.h", bycopy.} = object
+  AffSrc* {.importc: "AFF_SRC", header: "tonc.h", bycopy, completeStruct.} = object
     ## Simple scale-rotation source struct.
-    ## This can be used with ObjAffineSet, and several of tonc's affine functions
+    ## This can be used with ``bios.ObjAffineSet``, and several of tonc's affine functions
     sx* {.importc: "sx".}: int16          ## Horizontal zoom (8.8f)
     sy* {.importc: "sy".}: int16          ## Vertical zoom (8.8f)
-    alpha* {.importc: "alpha".}: uint16   ## Counter-clockwise angle (range [0, 0xFFFF])
+    alpha* {.importc: "alpha".}: uint16   ## Counter-clockwise angle (range 0..0xffff)
   
-  AffSrcEx* {.importc: "AFF_SRC_EX", header: "tonc.h", bycopy.} = object
+  AffSrcEx* {.importc: "AFF_SRC_EX", header: "tonc.h", bycopy, completeStruct.} = object
     ## Extended scale-rotate source struct
     ## This is used to scale/rotate around an arbitrary point. See tonc's main text for all the details.
     texX* {.importc: "tex_x".}: int32   ## Texture-space anchor, x coordinate  (.8f)
@@ -63,7 +63,7 @@ type
     sy* {.importc: "sy".}: int16        ## Vertical zoom (8.8f)
     alpha* {.importc: "alpha".}: uint16 ## Counter-clockwise angle (range [0, 0xFFFF])
   
-  AffDst* {.importc: "AFF_DST", header: "tonc.h", bycopy.} = object
+  AffDst* {.importc: "AFF_DST", header: "tonc.h", bycopy, completeStruct.} = object
     ## Simple scale-rotation destination struct, BG version.
     ## This is a P-matrix with continuous elements, like the BG matrix.
     ## It can be used with ObjAffineSet.
@@ -72,7 +72,7 @@ type
     pc* {.importc: "pc".}: int16
     pd* {.importc: "pd".}: int16
 
-  AffDstEx* {.importc: "AFF_DST_EX", header: "tonc.h", bycopy.} = object
+  AffDstEx* {.importc: "AFF_DST_EX", header: "tonc.h", bycopy, completeStruct.} = object
     ## Extended scale-rotate destination struct
     ## This contains the P-matrix and a fixed-point offset, the
     ##  combination can be used to rotate around an arbitrary point.
@@ -97,20 +97,20 @@ type
 
 type
   BgPoint* = Point16
-  Point16* {.importc: "POINT16", header: "tonc.h", bycopy.} = object
+  Point16* {.importc: "POINT16", header: "tonc.h", bycopy, completeStruct.} = object
     x*, y*: int16
 
 type BgAffine* = AffDstEx
   ## Affine parameters for backgrounds; range : 0400:0020 - 0400:003F
 
 type
-  DmaRec* {.importc: "DMA_REC", header: "tonc.h", bycopy.} = object
+  DmaRec* {.importc: "DMA_REC", header: "tonc.h", bycopy, completeStruct.} = object
     ##  DMA struct; range: 0400:00B0 - 0400:00DF
     src* {.importc: "src".}: pointer
     dst* {.importc: "dst".}: pointer
     cnt* {.importc: "cnt".}: uint32
   
-  TmrRec* {.importc: "TMR_REC", header: "tonc.h", bycopy.} = object
+  TmrRec* {.importc: "TMR_REC", header: "tonc.h", bycopy, completeStruct.} = object
     ## Timer struct, range: 0400:0100 - 0400:010F
     ## note: The attribute is required, because union's counted as u32 otherwise.
     start* {.importc: "start".}: uint16
@@ -134,25 +134,25 @@ type
   Charblock8* = array[256, Tile8]
 
 type
-  ObjAttr* {.importc: "OBJ_ATTR", header: "tonc.h", bycopy.} = object
-    ## Object attributes.
-    ## Note:
+  ObjAttr* {.importc: "OBJ_ATTR", header: "tonc.h", bycopy, completeStruct.} = object
+    ## Object attributes. i.e. a sprite.
+    ## **Note**
     ##  The `fill` field is padding for the interlace with ObjAffine.
     ##  It will not be copied when assigning one ObjAttr to another.
     attr0* {.importc: "attr0".}: uint16
     attr1* {.importc: "attr1".}: uint16
     attr2* {.importc: "attr2".}: uint16
-    # fill {.importc: "fill".}: int16
+    fill {.importc: "fill".}: int16
   
-  ObjAffine* {.importc: "OBJ_AFFINE", header: "tonc.h", bycopy.} = object
+  ObjAffine* {.importc: "OBJ_AFFINE", header: "tonc.h", bycopy, completeStruct.} = object
     ## Object affine parameters.
-    # fill0 {.importc: "fill0".}: array[3, uint16]
+    fill0 {.importc: "fill0".}: array[3, uint16]
     pa* {.importc: "pa".}: int16
-    # fill1 {.importc: "fill1".}: array[3, uint16]
+    fill1 {.importc: "fill1".}: array[3, uint16]
     pb* {.importc: "pb".}: int16
-    # fill2 {.importc: "fill2".}: array[3, uint16]
+    fill2 {.importc: "fill2".}: array[3, uint16]
     pc* {.importc: "pc".}: int16
-    # fill3 {.importc: "fill3".}: array[3, uint16]
+    fill3 {.importc: "fill3".}: array[3, uint16]
     pd* {.importc: "pd".}: int16
   
   ObjAttrPtr* = ptr ObjAttr
@@ -175,29 +175,3 @@ proc `=`*(dst: var ObjAffine, src: ObjAffine) {.inline.} =
   dst.pc = src.pc
   dst.pd = src.pd
 
-
-# sizeof doesn't work with imported object types at compile time
-# and the {.size:X.} pragma is only intended for enums, so we can't reliably specify the size ourselves
-# so we'll have to make do with these constants for now...
-const
-  sizeof_Block* = 8 * sizeof(uint32)
-  sizeof_Tile* = 8 * sizeof(uint32)
-  sizeof_Tile8* = 16 * sizeof(uint32)
-  sizeof_AffSrc* = 3 * sizeof(int16)
-  sizeof_AffSrcEx* = 2 * sizeof(int32) + 5 * sizeof(int16)
-  sizeof_AffDst* = 4 * sizeof(int16)
-  sizeof_AffDstEx* = 4 * sizeof(int16) + 2 * sizeof(int32)
-  sizeof_Point16* = 2 * sizeof(int16)
-  sizeof_DmaRec* = 2 * sizeof(pointer) + sizeof(uint32)
-  sizeof_TmrRec* = 3 * sizeof(uint16)
-  sizeof_Palbank* = 16 * sizeof(Color)
-  sizeof_Screenline* = 32 * sizeof(ScrEntry)
-  sizeof_ScreenMat* = 32 * 32 * sizeof(ScrEntry)
-  sizeof_Screenblock* = 1024 * sizeof(ScrEntry)
-  sizeof_M3Line* = 240 * sizeof(Color)
-  sizeof_M4Line* = 240 * sizeof(uint8)
-  sizeof_M5Line* = 160 * sizeof(Color)
-  sizeof_Charblock* = 512 * sizeof_Tile
-  sizeof_Charblock8* = 256 * sizeof_Tile8
-  sizeof_ObjAttr* = 8
-  sizeof_ObjAffine* = 8
