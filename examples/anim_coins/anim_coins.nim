90cbecdf0369b8a56d4a840d7cd2dcee8738f4be
gecko <geckojsc@gmail.com>
2021-04-06 23:17:25 +0100

    Minor style tweaks

--- examples/anim_coins/anim_coins.nim
+++ examples/anim_coins/anim_coins.nim
@@ -84,7 +84,7 @@ var coins {.noinit.}: array[40, Coin]
 proc main() =
   
   irq.init()
-  irq.enable(iiVBLANK)
+  irq.enable(iiVBlank)
   
   # enable sprites with 1d mapping
   dispcnt.init:
@@ -92,10 +92,10 @@ proc main() =
     obj1d = true
   
   # copy palette into object PAL RAM
-  memcpy16(addr palObjBank[pal], addr coinPal, coinPal.len)
+  memcpy16(addr objPalMem[pal], addr coinPal, coinPal.len)
   
   # copy all frames into object VRAM
-  memcpy32(addr tileMemObj[0][tid], addr coinTiles, coinTiles.len)
+  memcpy32(addr objTileMem[tid], addr coinTiles, coinTiles.len)
   
   # initialize coins
   for coin in mitems(coins):
@@ -119,7 +119,7 @@ proc main() =
     
     # hide remaining sprites
     while oid < oamMem.len:
-      oamMem[oid].hide()
+      objMem[oid].hide()
       inc oid
   
 
