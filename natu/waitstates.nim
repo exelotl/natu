06a36a166b07cc031d3869ed7b7cbc8f46a26891
Jeremy Clarke <gecko@exelo.tl>
2022-03-21 23:30:52 +0000

    Rename waitstate enum values & improve docs

--- natu/waitstates.nim
+++ natu/waitstates.nim
@@ -6,10 +6,10 @@
 ## 
 ## .. code-block:: nim
 ##   waitcnt.init(
-##     sram = wss_8,      # 8 cycles to access SRAM.
-##     rom0 = ws0_n3s1,   # 3 cycles to access ROM, or 1 cycle for sequential access.
-##     rom2 = ws2_n8s8,   # 8 cycles to access ROM (mirror #2) which may be used for flash storage.
-##     prefetch = true    # prefetch buffer enabled.
+##     sram = WsSram.N8_S8,   # 8 cycles to access SRAM.
+##     rom0 = WsRom0.N3_S1,   # 3 cycles to access ROM, or 1 cycle for sequential access.
+##     rom2 = WsRom2.N8_S8,   # 8 cycles to access ROM (mirror #2) which may be used for flash storage.
+##     prefetch = true        # prefetch buffer enabled.
 ##   )
 ## 
 ## In this example, `waitcnt.rom0` determines the access time for the default view into ROM.
@@ -20,9 +20,9 @@
 ## 
 ## .. code-block:: nim
 ##   if slowGamePak():
-##     waitcnt.init(rom0 = ws0_n4s2)
+##     waitcnt.init(rom0 = WsRom0.N4_S2)
 ##   else:
-##     waitcnt.init(rom0 = ws0_n3s1)
+##     waitcnt.init(rom0 = WsRom0.N3_S1)
 ## 
 ## There are two additional ROM mirrors located at `0x0A000000` and `0x0C000000`,
 ## which have access times determined by `waitcnt.rom1` and `waitcnt.rom2`.
@@ -30,60 +30,75 @@
 ## The mirrors may be useful for carts containing multiple ROM chips. For example
 ## on some carts the upper 128KiB of ROM is mapped to flash storage which would
 ## require different access timings.
+## 
+## .. note::
+##    On Nim 1.6 and later, you can omit the waitstate enum prefixes as long as you enable
+##    `overloadable enums <https://nim-lang.org/1.6.0/manual.html#types-overloadable-enum-field-names>`_.
 
 import ./core
 import ./private/utils
 import std/volatile
 
+when (NimMajor, NimMinor) >= (1, 6):
+  {.experimental: "overloadableEnums".}
+  {.pragma: overloadable.}
+else:
+  {.pragma: overloadable, pure.}
+
 type
-  WsSram* = enum
+  WsSram* {.overloadable.} = enum
     ## SRAM access timings
-    wss_4  ## SRAM access takes 4 cycles
-    wss_3  ## SRAM access takes 3 cycles
-    wss_2  ## SRAM access takes 2 cycles
-    wss_8  ## SRAM access takes 8 cycles
+    N4_S4  ## SRAM access takes 4 cycles
+    N3_S3  ## SRAM access takes 3 cycles
+    N2_S2  ## SRAM access takes 2 cycles
+    N8_S8  ## SRAM access takes 8 cycles
   
-  WsRom0* = enum
+  WsRom0* {.overloadable.} = enum
     ## ROM access timings.
     ## 
-    ## Initial access to ROM takes `n` cycles, sequential access takes `s` cycles.
-    ws0_n4s2
-    ws0_n3s2
-    ws0_n2s2
-    ws0_n8s2
-    ws0_n4s1
-    ws0_n3s1
-    ws0_n2s1
-    ws0_n8s1
+    ## Initial access to ROM takes `N` cycles, sequential access takes `S` cycles.
+    ## 
+    ## 
+    ## For more information on `N` cycles and `S` cycles, see the `asm chapter
+    ## <https://www.coranac.com/tonc/text/asm.htm#ssec-misc-cycles>`_ of Tonc.
+    N4_S2
+    N3_S2
+    N2_S2
+    N8_S2
+    N4_S1
+    N3_S1
+    N2_S1
+    N8_S1
   
-  WsRom1* = enum
+  WsRom1* {.overloadable.} = enum
     ## Access timings for ROM mirror starting at `0x0A000000`.
-    ws1_n4s4
-    ws1_n3s4
-    ws1_n2s4
-    ws1_n8s4
-    ws1_n4s1
-    ws1_n3s1
-    ws1_n2s1
-    ws1_n8s1
+    N4_S4
+    N3_S4
+    N2_S4
+    N8_S4
+    N4_S1
+    N3_S1
+    N2_S1
+    N8_S1
   
-  WsRom2* = enum
+  WsRom2* {.overloadable.} = enum
     ## Access timings for ROM mirror starting at `0x0C000000`.
-    ws2_n4s8
-    ws2_n3s8
-    ws2_n2s8
-    ws2_n8s8
-    ws2_n4s1
-    ws2_n3s1
-    ws2_n2s1
-    ws2_n8s1
+    N4_S8
+    N3_S8
+    N2_S8
+    N8_S8
+    N4_S1
+    N3_S1
+    N2_S1
+    N8_S1
   
   WsPhi* = enum
-    ## PHI Terminal Output
-    wsPhiOff    ## Disabled (recommended because I have no idea what this is)
-    wsPhi4MHz   ## 4.19MHz 
-    wsPhi8MHz   ## 8.38MHz
-    wsPhi17MHz  ## 16.78MHz
+    ## PHI Terminal Output. This allows the GBA to supply a clock signal to the
+    ## cartridge hardware, but is not used in practise.
+    phiOff    ## Disabled (recommended)
+    phi4MHz   ## 4.19MHz 
+    phi8MHz   ## 8.38MHz
+    phi17MHz  ## 16.78MHz
   
   WaitCnt* {.bycopy, exportc.} = object
     ## Waitstate control
@@ -121,17 +136,17 @@ proc slowGamePak*: bool {.codegenDecl:EWRAM_CODE.} =
   var a, b, c: array[3, uint32]
   
   # Read forwards with slow access:
-  waitcnt.init(rom0 = ws0_n4s2)
+  waitcnt.init(rom0 = WsRom0.N4_S2)
   for i in countup(0,2):
     a[i] = volatileLoad(unsafeAddr data[i])
   
   # Read forwards with fast access:
-  waitcnt.init(rom0 = ws0_n3s1)
+  waitcnt.init(rom0 = WsRom0.N3_S1)
   for i in countup(0,2):
     b[i] = volatileLoad(unsafeAddr data[i])
   
   # Read backwards with fast access:
-  waitcnt.init(rom0 = ws0_n3s1)
+  waitcnt.init(rom0 = WsRom0.N3_S1)
   for i in countdown(2,0):
     c[i] = volatileLoad(unsafeAddr data[i])
   
